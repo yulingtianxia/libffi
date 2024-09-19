d2d6e7b151b896ccdd36213ea9d05afa277a453d
rebootyang <rebootyang@tencent.com>
2024-09-19 21:39:01 +0800

    add podspec and example

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by 杨萧玉 on 2024/9/19.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
